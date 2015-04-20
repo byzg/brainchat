8f9a3a965c217af2cca029e30d750246aabbbea0
byzg <byzg@mail.ru>
2015-04-21 02:28:28 +0300

    fix chat show 2.2

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,9 @@ class Message < ActiveRecord::Base
   belongs_to :user
   validates :text, :user_id, :chat_id, presence: true
   scope :available_messages, lambda { |chat, user| user.is_owner?(chat) ? chat.messages : chat.messages.where(user_id: [user.id, chat.owner_id]).sort_by(&:created_at) }
+
+  def text
+    super.gsub("\n", "<br>").html_safe
+  end
+
 end
